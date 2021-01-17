dda5338743732abf8265afefa9d6010640485606
zwj <923551363@qq.com>
2021-01-17 10:28:47 +0800

    commit

--- xorg-install.sh
+++ xorg-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pacman -Sy
-pacman -S xorg xorg-server xorg-apps xorg-xinit xorg-xrandr nvidia xf86-video-intel --noconfirm
+pacman -S xorg xorg-server xorg-apps xorg-xinit xorg-xrandr nvidia xf86-video-intel arandr --noconfirm
 
 exit 0
