18cabb78801000c4ef20dfd8f5501a906577ace9
Nobume <923551363@qq.com>
2021-11-05 08:14:27 +0800

    some shell

---
+++ alsa-install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+pacman -Sy alsa-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth --noconfirm
---
+++ fonts-install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+pacman -Sy adobe-source-han-serif-cn-fonts wqy-zenhei noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts-fira-code --noconfirm
