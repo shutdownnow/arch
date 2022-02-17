edc100e6a2e7b88bbd816038e2c9443e471c81f6
Nobume <923551363@qq.com>
2022-02-17 15:28:23 +0800

    base and fonts

--- base-install.sh
+++ base-install.sh
@@ -2,8 +2,8 @@
 cp ./config/mirrorlist /etc/pacman.d/mirrorlist
 cp ./config/pacman.conf /etc/pacman.conf
 timedatectl set-ntp true
-pacstrap /mnt base base-devel linux linux-firmware dhcpcd iwd neovim sudo bash-completion
+pacstrap /mnt base base-devel linux linux-firmware dhcpcd neovim sudo bash-completion
 genfstab -U /mnt >> /mnt/etc/fstab
 cp ./config/pacman.conf /mnt/etc/pacman.conf
 cp ./config/grub /mnt/root/grub
-arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime;hwclock --systohc;echo -e 'en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8' >> /etc/locale.gen;locale-gen;echo 'LANG=en_US.UTF-8' >> /etc/locale.conf;echo 'Arch' >> /etc/hostname;echo -e '127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\tArch.localdomain\tArch' >> /etc/hosts;pacman -Sy grub efibootmgr --noconfirm;mv /root/grub /etc/default/grub;grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB;grub-mkconfig -o /boot/grub/grub.cfg;systemctl enable dhcpcd iwd;useradd -m -G wheel -s /bin/bash nobume"
+arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime;hwclock --systohc;echo -e 'en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8' >> /etc/locale.gen;locale-gen;echo 'LANG=en_US.UTF-8' >> /etc/locale.conf;echo '$1' >> /etc/hostname;echo -e '127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t$1.localdomain\t$1' >> /etc/hosts;pacman -Sy grub efibootmgr --noconfirm;mv /root/grub /etc/default/grub;grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB;grub-mkconfig -o /boot/grub/grub.cfg;systemctl enable dhcpcd;useradd -m -G wheel -s /bin/bash nobume"
--- fonts-install.sh
+++ fonts-install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pacman -Sy adobe-source-han-serif-cn-fonts wqy-zenhei noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts-fira-code --noconfirm
+pacman -Sy adobe-source-han-serif-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-mono-cn-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nerd-fonts-fira-code ttf-linux-libertine ttf-inconsolata ttf-joypixels noto-fonts-emoji ttf-liberation ttf-droid wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei --noconfirm
